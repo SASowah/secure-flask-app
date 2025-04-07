294e268b472b19359e247ddb49b409c0eea83e03
Samuel <samniisowah@yahoo.com>
2025-04-07 18:02:36 -0400

    update

--- src/init.db.js
+++ src/init.db.js
@@ -3,4 +3,5 @@ db.messages.insertMany([
   { text: "Hello from MongoDB!" },
   { text: "This app uses HTTPS via Let's Encrypt" },
   { text: "Deployed securely with Ansible and Jenkins" }
+  { text: "Deployed securely with Ansible and Jenkins" }
 ]);
